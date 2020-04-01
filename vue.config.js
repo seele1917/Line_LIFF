a0bd22e21227017ae8a9cd2642ba1d084fc65853
= <=>
2020-04-02 02:42:19 +0900

    prototype finish

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-  publicPath: '/line_liff/',
+  publicPath: '/',
   devServer: {
     public: "https://a60c41fb.ngrok.io",
     disableHostCheck: true
